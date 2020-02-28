4b54b07be06e98f07680d81faa64cf06834de87d
Louis Ritter <louisritter@Louiss-MacBook-Pro.local>
2020-02-28 10:16:35 -0500

    init

---
+++ src/Movie.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./Movie.css";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+class Movie extends React.Component {
+   render() {
+       const {title, image, year} = this.props;
+       return (
+           <Card style={{ width: '18rem', backgroundColor: '#282c34' }} className="movie">
+               <Card.Body>
+                <div className="title-year">
+                    <Card.Title className="title">{title}</Card.Title>
+                    <Card.Subtitle className="year">{year}</Card.Subtitle>
+                </div>
+               </Card.Body>
+               
+                   <Card.Img src={image.url} alt="my movie poster"/>
+           </Card>
+       )
+   }
+}
+export default Movie;
