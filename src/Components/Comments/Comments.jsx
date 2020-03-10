2a6bdafa883928ffc1079c1dbf720fda730fc5bd
Louis Ritter <louritter1@gmail.com>
2020-03-10 11:57:22 -0400

    added comment form and submitting comments, login not functional, adding log in form

--- src/Components/Comments/Comments.jsx
+++ src/Components/Comments/Comments.jsx
@@ -4,6 +4,9 @@ import {Switch, Link} from "react-router-dom"
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import firebase, { auth, provider, db } from "../../firebaseConfig.js"
+import Form from 'react-bootstrap/Form';
+
+
 
 class Comments extends React.Component {
   
@@ -16,8 +19,14 @@ class Comments extends React.Component {
 
     }
     componentDidMount() {
+      const ref = db
+      .collection('MovieComments')
+      .doc(this.props.movieid)
+      .collection("comments");
+      this.setState({ref});
         auth.onAuthStateChanged((user) => {
           if (user) {
+            console.log(user);
             this.setState({ user });
           } 
         });
@@ -32,20 +41,56 @@ class Comments extends React.Component {
           
 
         });
+
         
     }
+
+    submitCommentHandler = (event) => {
+      event.preventDefault();
+      const cmnt = {
+        
+      };
+
+      cmnt.comment = this.state.comment;
+      cmnt.userid = this.state.user.displayName;
+      cmnt.replies = [];
+
+      db.collection("MovieComments")
+      .doc(this.props.movieid)
+      .collection("comments")
+      .add(cmnt);
+      alert("You are submitting " + this.state.comment);
+    }
+
+    handleChange = e => {
+      this.setState({comment: e.target.value});
+    };
 
 
 
     render() {
-
 
        return (
          
 
         
         <div className="Comments">
-          <h1>Comments</h1>
+          <div className = "commentwritebox">
+            <h3>Discuss The Movie Here</h3>
+            <Form onSubmit={this.submitCommentHandler}>
+            <Form.Group controlId="commentForm.ControlTextarea1">
+              <Form.Control as="textarea" 
+               type="text"
+               value={this.state.value}
+               placeholder="Enter text"
+               onChange={this.handleChange}
+               rows="3" />
+            </Form.Group>
+               <Button type="submit">Submit</Button>
+            </Form>
+          </div>
+          <div className="commentlistbox">
+          <h3>Comments</h3>
                 {this.state.user ?
                     <div>
                       {this.state.comments.map((comment, key) =>(
@@ -70,8 +115,7 @@ class Comments extends React.Component {
                     <h3>Log in to see Comments</h3>
                   </div>
                 }
-         
-            
+         </div>
         </div>  
         
        )
